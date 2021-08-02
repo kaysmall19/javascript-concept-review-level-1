dc789653d059e95369be8ec30f472a58258271e6
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-08-02 14:22:36 -0500

    commit on main

---
+++ main.js
@@ -0,0 +1,21 @@
+function main(){
+    
+}
+
+// EXCERISE 1: Integer Numbers Range
+// create a function with integer parameters x1 and x2.
+// return that function
+
+// Excerise 2: Multiplications Table
+// create a function that writes in the console a multiplicstion table from 1 to 10
+
+// EXCERISE 3: Multiplications table on demand
+// create a funcion that writes in the console a multiplication table of any number passed the parameter.
+
+//EXCERISE 4: Calculations and operators
+// create a program that writes in the console the multiples of 23 but less than 500 and end ith a sum of all.
+
+// Excerise 5: Write your own max() function.
+// create a function called max().
+//Use two numbers that arguments and returns
+// create if-then-else statement
